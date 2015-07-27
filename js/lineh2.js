6960a26df1512778392fdfc1464ab0e8edb9cbab
Chad A. Carino <chadc@tekserve.com>
2015-07-27 13:43:27 -0400

    added direct web symbols font to overcome 401 issues on pre-staging, updated settings file, added js fix for home page display (really a shortcode issue, but only an issue in one place atm), fixed mobile width extending past window width (.row .row), updated some styles for consistency

--- js/lineh2.js
+++ js/lineh2.js
@@ -11,5 +11,19 @@ var $j = jQuery;
 $j(function() {
 
 	$j('.tekserve-studio.page .content article h2, .tekserve-studio.page .content .item-wrap-slug-h2 h2').wrap('<div class="h2container" />');
+	if ($j('body').hasClass('home')) {
+	
+		var $title = $j('.category-title');
+		$title.parents('.row').remove();
+		var $content = $j('#velocity-page-wrapper');
+		if ($content) {
+		
+			var $row = $content.closest('.row');
+			var shifted = $content.detach();
+			$row.prepend(shifted);
+		
+		}
+	
+	}	//end if ($j('body').hasClass('home'))
 
 }); //end $j(function()
